6d8db35a1fdec536e35e367c6de7837a7708b330
Antonio Kobashikawa <akobashikawa@gmail.com>
2025-05-13 16:57:57 -0500

    feat: render index

--- js/components/App.js
+++ js/components/App.js
@@ -1,30 +1,40 @@
 import { ref, useTemplateRef, onMounted, watch, nextTick } from 'vue';
 import axios from 'axios';
+import config from '../config.js';
 
 const App = {
+    components: {
+    },
 
-	components: {
-	},
+    template: `
+    <div class="container">
+        <h1>GitHub CMS</h1>
 
-	template: `
-	<h1>GitHub CMS</h1>
+        <div v-html="content"></div>
 
-
-	<footer>
-		<em><a href="https://github.com/akobashikawa/github-cms-101" target="_blank">@GitHub></a></em>
-	</footer>
+        <footer>
+            <em><a href="https://github.com/akobashikawa/github-cms-101" target="_blank">@GitHub></a></em>
+        </footer>
+    </div>
     `,
 
-	setup() {
-		
-		onMounted(() => {
-		});
+    setup() {
+        const content = ref('');
+        
+        onMounted(async () => {
+            try {
+                const response = await axios.get(`${config.pagesBaseUrl}/index.md`);
+                content.value = response.data;
+            } catch (error) {
+                console.error('Error loading content:', error);
+                content.value = 'Error loading content';
+            }
+        });
 
-		return {
-			
-		}
-	},
-
+        return {
+            content
+        }
+    },
 };
 
 export default App;
---
+++ js/config.js
@@ -0,0 +1,5 @@
+const config = {
+  pagesBaseUrl: 'https://raw.githubusercontent.com/akobashikawa/github-cms-101/refs/heads/master/pages',
+};
+
+export default config;
